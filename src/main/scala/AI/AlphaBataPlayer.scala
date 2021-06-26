2a0da733c6c3e5e7bd681c827dcff432905fc701
YusukeFukushima <fukushy1998@gmail.com>
2021-06-26 13:32:17 +0900

    [update] add MAX_TURNS and others

--- src/main/scala/AI/AlphaBataPlayer.scala
+++ src/main/scala/AI/AlphaBataPlayer.scala
@@ -2,11 +2,34 @@ package AI
 import Central.Board
 
 class AlphaBataPlayer extends {
-  val preSearchDepth = 3
-  val normalDepth = 5
-  val wldDepth = 15
-  val perfectDepth = 13
+  val pre_search_depth = 3
+  val normal_depth = 5
+  val wld_depth = 15
+  val perfect_depth = 13
 } with ComputerPlayer {
-  def move(board: Board): Unit = {
+  def move(board: Board): (Int, Int) = {
+    val movables = board.movable_pos
+
+    // パス判定：Masterクラスで実装済み
+
+    if(movables.size == 1){
+      return movables.head
+    }
+
+    var limit: Int = 0
+
+    if(board.MAX_TURNS - board.turn_count <= wld_depth){
+      limit = Int.MaxValue
+    }else{
+      limit = normal_depth
+    }
+
+    var eval, eval_max = Int.MinValue
+
+    for(move <- movables){
+      board.putPiece(move._1, move._2)
+    }
+    val dummy_x, dummy_y = 0
+    (dummy_x, dummy_y)
   }
 }
--- src/main/scala/AI/ComputerPlayer.scala
+++ src/main/scala/AI/ComputerPlayer.scala
@@ -4,10 +4,9 @@ import Central.Board
 
 trait ComputerPlayer {
 
-  val preSearchDepth: Int
-  val normalDepth: Int
-  val wldDepth: Int
-  val perfectDepth: Int
+  val pre_search_depth: Int // alpha-bata法, NegaScout法において、事前に手を調べて探索順序を決めるための先読み手数
+  val normal_depth: Int    // 序盤・中盤の探索における先読み手数
+  val wld_depth: Int       // 終盤において、必勝読みを始める残り手数(wld = win, lose, draw)
+  val perfect_depth: Int   // 終盤において完全よみを始める残り手数
 
-  def move(board: Board): Unit
 }
